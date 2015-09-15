004c62658ad323ca442e6583288e62d730e197ad
Ygorsunny Jean <ygorsunnyjean@1x-vl919-72-19-117-247.wireless.umass.edu>
2015-09-15 14:00:54 -0400

    Initial Commit

---
+++ Lab0/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Lab0
+//
+//  Created by Ygorsunny Jean on 9/15/15.
+//  Copyright (c) 2015 Ygorsunny Jean. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
