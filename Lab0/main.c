b93ab88ed4b3bb4022797ddf1839b5fcac7a2891
Ygorsunny Jean <ygorsunnyjean@1x-vl919-72-19-116-171.wireless.umass.edu>
2015-09-17 15:08:04 -0400

    So Square 1 Maybe

--- Lab0/main.c
+++ Lab0/main.c
@@ -7,15 +7,25 @@
 
 int main(int argc, const char * argv[]) {
 	// insert code here...
+	FILE *ifp;
+	FILE *ofp;
 	
+	ifp = fopen("./plainTextFile.txt", "r");
+	ofp = fopen("./outputFile.txt", "w");
+
 	int i;
 	char S[i];
+	
+	
+	
 	
 	for (i=0; i<256; i++){
 		S[i]=i;
 		printf("%i ",i);
 	}
-//somehow
+	
+	assert(ifp != NULL);
+	assert(ofp != NULL);
 	/*initialized an array with 256 entries for S*/
     return 0;
 }
