6821c6f54cfa40752f4b28878aa5a03c35dccc5c
Ygorsunny Jean <ygorsunnyjean@1x-vl919-72-19-117-180.wireless.umass.edu>
2015-09-17 12:48:13 -0400

    Can I type this?

--- Lab0/main.c
+++ Lab0/main.c
@@ -7,22 +7,15 @@
 
 int main(int argc, const char * argv[]) {
 	// insert code here...
+	
 	int i;
-	
-	
 	char S[i];
 	
 	for (i=0; i<256; i++){
 		S[i]=i;
 		printf("%i ",i);
 	}
-	
-	printf("Hello, World!\n");
-	printf("Hello, World2!\n");
-	printf("Hello, World3!\n");
-	printf("Hello, World4!\n");
-	printf("test 5");
-	printf("pre-command line");
-	printf("command line");
+	/*initialized an array with 256 entries for S*/
+
     return 0;
 }
