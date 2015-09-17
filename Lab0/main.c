69998488037aeae5dc8d24bf15ea430c0bdf2f16
Ygorsunny Jean <ygorsunnyjean@1x-vl919-72-19-116-171.wireless.umass.edu>
2015-09-17 15:20:05 -0400

    Try again

--- Lab0/main.c
+++ Lab0/main.c
@@ -4,6 +4,9 @@
 
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
 
 int main(int argc, const char * argv[]) {
 	// insert code here...
@@ -14,7 +17,7 @@ int main(int argc, const char * argv[]) {
 	ofp = fopen("./outputFile.txt", "w");
 
 	int i;
-	char S[i];
+	unsigned char S[i];
 	
 	
 	
@@ -29,3 +32,51 @@ int main(int argc, const char * argv[]) {
 	/*initialized an array with 256 entries for S*/
     return 0;
 }
+/*
+//initialize T.
+unsigned char T[256];
+//go through T setting T[i] = key[i mod 256]
+for(i=0; i<256; i++)
+{
+	//note that for 0<=i<256, i mod kLength = i, so T=key if they're the same length or key is longer. Not a mistake.
+	T[i] = key[i % kLength];
+}
+
+//permute S to get everything set up.
+int j=0;
+for(i=0; i<256; i++)
+{
+	//set up j
+	j = (j + (int)S[i] + (int)T[i]) % 256;
+	//swap S[i] and S[j]
+	char S_ti = S[i];
+	char S_tj = S[j];
+	S[i] = S_tj;
+	S[j] = S_ti;
+}
+
+//prints the shit out: this is just for testing
+printf("S: ");
+for(i=0; i<256; i++)
+{
+	printf("%c", S[i]);
+	printf(", ");
+}
+printf("\n\n");
+
+printf("T: ");
+for(i=0; i<256; i++)
+{
+	printf("%c", T[i]);
+	printf(", ");
+}
+printf("\n\n");
+
+printf("key: ");
+for(i=0; i<256; i++)
+{
+	printf("%c", key[i]);
+	printf(", ");
+}
+printf("\n");
+ */
