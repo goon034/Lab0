d1bb336db7827d4cc96a0db8354aa71f56d723ff
Emma Bryce <EmmaBryce@1x-vl915-72-19-125-115.wireless.umass.edu>
2015-09-19 18:19:15 -0400

    l

--- Lab0/main.c
+++ Lab0/main.c
@@ -8,59 +8,61 @@
 #include <math.h>
 #include <assert.h>
 
+char generateKeyByte(char *s);
+
 int main(int argc, const char * argv[]) {
-
+	
 	//test key and length. this should be removed once the I/O's running.
-	unsigned char key[256] = {'£', '\x82', 'o', 'ä', '\x0c', '\n', '5', 'å', '\x96', '\x1f', 'Á', 'Ý', 'k', '\x89', '\x87', 'ä', '_', '\x9b', 'î', '\x86', 'f', 'o', 'Ë', 'H', '\x15', '\x97', '\x17', 'S', 'þ', '.', 'b', 'B', '\x81', 'ã', '+', '\x8b', '@', '£', '\x8c', '¥', '3', '®', '\x08', 'æ', 'f', '!', 'Ë', 'ú', 'g', '\x8a', '(', '\x87', 'A', '\x15', '\x0e', 'Í', '~', '¹', '\x17', '¦', 'ú', '\x84', 'ö', 'Q', '\x10', '3', '|', 'B', 'j', '\x82', 'Ç', 'ª', '\x86', '\x19', '*', 'Ï', '÷', 'a', '\x0e', '\x03', 'Ë', 'Ã', 'N', 'Z', 'ÿ', 'x', '~', 'y', '\x0f', 'ê', 'P', 'J', '\\', '\x95', '\x83', 'æ', '·', '¾', 'Ü', 'U', 'é', 'Ë', 'ï', 'Ã', 'Ô', '!', 'ò', 'ë', 'É', '\x97', 'ß', '0', '*', '\x06', 'ý', 'j', 'µ', 'l', '»', '\x1e', '(', '\x83', '\n', 'S', 'ó', '\x98', 'w', 'i', 'Å', '\x02', 'E', 'Ì', '\x17', 'r', '¦', '¢', '^', 'Þ', '\x14', 'j', 'D', '«', '>', 'x', ']', 'ì', '\x06', '\x96', '\x86', ']', 'ê', '¤', '5', 'w', 'ú', '5', '6', 'Ï', 'û', 'C', 'ï', '$', '\x10', '\x99', 'ó', '|', '\x85', '\x94', '\x17', '¬', '\x19', 'R', '\x07', 'f', '=', 'õ', 'c', '\x1c', 'j', 'ó', 'B', 'É', '¡', '\x9b', '\x16', '\x14', '\x90', 'Ê', 'À', '=', 'à', '÷', 'ÿ', 'A', 'G', '\x98', '\x19', 'Q', 'Ù', '¤', 'Ã', '\\', '²', '\x16', '¡', '&', '\r', '\x01', '\\', '³', '¯', 'Î', '¹', 'È', 'v', 'B', 'n', 'z', 'Ú', 'b', 'P', 'Ø', '3', 'É', '\x91', 'u', 'B', '\x85', '¨', '¹', '\x1a', 'å', 'A', ']', '\x9a', 'E', '>', '\x86', '3', 'S', '¥', '_', '¢', ')', 'Ô', 'ò', '|', 'Ø', '[', '\x7f', '÷', '\x1a', 'æ', '*', 'P', '³'};
+	unsigned char key[256] = {'f', 'l', 'o', 'w', 'e', 'r', 's', ' ', 'a', 'r', 'e', ' ', 'c', 'o', 'm', 'p', 'l', 'e', 't', 'e', 'l', 'y', ' ', 'l', 'o', 'o', 'v', 'e', 'l', 'y', '.', ' ', 'f', 'l', 'o', 'w', 'e', 'r', 's', ' ', 'l', 'o', 'o', 'k', ' ', 'l', 'i', 'k', 'e', 'a', 's', 'd', 'f', 'g', 'h', '!', '@', '#', '$', '%', '^', '&', '*', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'a', 'l', 'i', 'e', 'n', 's', '.', ' ', 'T', 'h', 'e', ' ', 'b', 'i', 'g', 'e' , 'a', 'l', ' ', 'a', 'b', 'o', 'u', 't', ' ', 'f', 'l', 'o', 'w', 'e', 'r', 's', ' ', 'i', 's', ' ', 'b', 'o', 't', 'a', 'n', 'i', 'c', 'a', 'l', '.', ' ', 'E', 'v', 'e', 'r', 'y', 't', 'h', 'i', 'n', 'g', ' ', 'a', 'b', 'o', 'u', 't', ' ', 't', 'h', 'e', 'm', ' ', 's', 'u', 'c', 'k', 's', '.', 'x', 'x', 'x', 'x', '(', 'r', 'u', 'l', 'e', 's', ':', ')', ')', ' ', 's', 'e', 'n', 'd', 'm', 'e', ' ', '$', '7', '0', '0', ',', '0', '0', '0', ',', '0', '0', '0', ',', '0', '0', '0', ',', '0', '0', 'i', 'b', 'u', 't', 'i', 'o', 'n', ' ', 't', 'o', ' ', 't', 'h', 'e', ' ', 'p', 'r', 'o', 'j', 'e', 'k', 't', ' ', 'a', 'n', 'd', ' ', 'a', ' ', 'N', 'o', 'b', 'e', 'l', ' ', 'p', 'r', 'i', 'z', 'e', ' ', ' ', '.', '.', '.', '.', ')', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'};
 	int kLength_test = 256;
-
+	
 	//declare file pointers
 	FILE *kfp;
-	FILE *ifp;    
-	FILE *ofp;    
-   
+	FILE *ifp;
+	FILE *ofp;
+	
 	//declare the message/key lengths
 	int kLength;
 	int messageLength;
-
+	
 	//declare i and j (used in loops)
-	int i;  
+	int i;
 	int j;
  
 	//open the files. note that the input files should be in the same directory as the C file.
-	kfp = fopen("./keyFile.txt", "rb");
+	kfp = fopen("./keyFile.txt", "rb"); //read in binary: "rb"
 	ifp = fopen("./plainText.txt", "rb");
 	ofp = fopen("./encryptedText.txt", "w");
-
-
+	
+	
 	//make sure the files were found correctly
 	assert(kfp != NULL);
 	assert(ifp != NULL);
 	assert(ofp != NULL);
-
-    
+	
+	
 	//this block gets the message/key lengths.
 	//go to the end of the file
 	fseek(kfp, 0L, SEEK_END);
-	//ftell returns the position of the current char, which is the last one in the file 
-	kLength = ftell(kfp);
+	//ftell returns the position of the current char, which is the last one in the file
+	kLength = (int) ftell(kfp);
 	//return to the beginning
 	fseek(kfp, 0L, SEEK_SET);
 	//same as before
 	fseek(ifp, 0L, SEEK_END);
-	messageLength = ftell(ifp);
+	messageLength = (int) ftell(ifp);
 	fseek(ifp, 0L, SEEK_SET);
-
-
+	
+	
 	//initialize S
 	unsigned char S[256];
 	//fill S with [0, 1, 2, 3, ..., 255] as chars
 	for(i=0; i<256; i++)
-        {
-        	S[i] = (char)i;
-        }
-
-
+	{
+		S[i] = (char)i;
+	}
+	
+	
 	//initialize T.
 	unsigned char T[256];
 	//go through T setting T[i] = key[i mod kLength]
@@ -70,8 +72,8 @@ int main(int argc, const char * argv[]) {
 		//kLength_test should be replaced with kLength once the I/O stuff is working
 		T[i] = key[i % kLength_test];
 	}
-
-
+	
+	
 	//permute S to get everything set up.
 	j=0;
 	for(i=0; i<256; i++)
@@ -84,8 +86,8 @@ int main(int argc, const char * argv[]) {
 		S[i] = S_tj;
 		S[j] = S_ti;
 	}
-
-
+	
+	
 	//prints shit out: this is just for testing
 	printf("kLength: %d\n\n", kLength);
 	printf("messageLength: %d\n\n", messageLength);
@@ -121,12 +123,27 @@ int main(int argc, const char * argv[]) {
 		printf("%c", key[i]);
 		printf(", ");
 	}
-
 	
-
-
+	
+	
+	
 	//close the files
 	fclose(kfp);
 	fclose(ofp);
 	fclose(ifp);
+}
+
+char generateKeyByte(char *s){
+	static int i = 0;
+	static int j = 0;
+	static int t = 0;
+	i = (i+1) % 256;
+	j = (j + *(s+i)) % 256;
+	//Swap *(s+i) and *(s+j)
+	char s_ti = *(s+i);
+	char s_tj = *(s+i);
+	*(s+i) = s_tj;
+	*(s+j) = s_ti;
+	t = (*(s+i) + *(s+j)) % 256;
+	return *(s+t);
 }
